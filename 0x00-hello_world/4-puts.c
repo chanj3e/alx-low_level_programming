9e86987c4be0039aa9bae42facb70423673cc31d
omar atef <change0m0@gmail.com>
2023-03-16 17:35:55 +0200

    c prog

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putsf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
