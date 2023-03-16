edd7e6dbc2931af514aaa46b8054c3fb01a51d1c
omar atef <change0m0@gmail.com>
2023-03-16 17:52:30 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that print a text
- *
- * Return: Always 0 (Success)
+ * Return:  0 (Success)
  */
 int main(void)
 {
-	putsf("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
