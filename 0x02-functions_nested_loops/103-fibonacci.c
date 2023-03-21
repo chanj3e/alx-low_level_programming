487439342f6d4edc55e4b9ec31c5b2aa593b1ad9
omar atef <change0m0@gmail.com>
2023-03-21 15:42:55 +0200

    qw

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	long x = 1, y = 2, sum = y;
+
+	while (x + y < 4000000)
+	{
+		y += x;
+		if (y % 2 == 0)
+		{
+			sum += y;
+			x = y - x;
+			++i;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
