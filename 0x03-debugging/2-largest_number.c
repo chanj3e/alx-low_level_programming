24a8053ec02e712d4b5379de8dbd63aa133df878
omar atef <change0m0@gmail.com>
2023-03-22 14:33:09 +0200

    asdad

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#incldue "main.h"
+
+/**
+ * largest_number - returns the largest number
+ * @a: frst num
+ * @b: sec num
+ * @c: third num
+ * Return: larget number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
