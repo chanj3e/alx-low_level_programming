9d13f5dd90e45e687f8fdbfe72f667beff5bfe7d
omar atef <change0m0@gmail.com>
2023-04-04 00:03:26 +0200

    ASsa

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strstr - function
+ * @haystack: input
+ * @needle: input
+ * Return: sa
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+		if (*j == '\0')
+			return (haystack);
+	}
+	return (0);
+}
