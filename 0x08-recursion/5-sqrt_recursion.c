8fafaa7ed2061821ae83156f5beb1952c19758c3
omar atef <change0m0@gmail.com>
2023-04-06 01:32:22 +0200

    ASas

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,4 +20,11 @@ int _sqrt_recursion(int n)
 int _sqrt(int n, int i)
 {
 	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+	else if (sqrt == n)
+		return (i);
+	else
+		return (_sqrt(n, i + 1));
 }
