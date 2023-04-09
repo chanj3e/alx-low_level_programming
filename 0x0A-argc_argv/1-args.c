4e141c56a49e71df6abee53c0953c801cfee3408
omar atef <change0m0@gmail.com>
2023-04-09 22:07:18 +0200

    AS

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: array of pointers
+ * Return: 0
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	if (argc >= 0)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
