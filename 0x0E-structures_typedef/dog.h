226754698955737073f3f21d317e8dcfce00996d
omar atef <change0m0@gmail.com>
2023-04-18 01:47:43 +0200

    ASa

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's basics information
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif
