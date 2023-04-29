9f6758d9c774147fcc6348acd2962b0d36c6a6f1
Omar atef <change0m0@gmail.com>
2023-04-29 02:16:36 +0200

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new)
 		return (NULL);
 	new->str = strdup(str);
-	new->i = i;
+	new->len = i;
 	new->next = (*head);
 	(*head) = new;
 	return (*head);
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -21,7 +21,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (!new)
 		return (NULL);
 	new->str = strdup(str);
-	new->i = i;
+	new->len = i;
 	new->next = NULL;
 	if (*head == NULL)
 	{
